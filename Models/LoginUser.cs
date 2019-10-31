7a690462a438bb5a15de3f91ca99a41c6a40e7b0
Rafael Tongson <rtongson@peppymiint.local>
2019-10-30 20:40:24 -0700

    make LoginUser model

--- Models/LoginUser.cs
+++ Models/LoginUser.cs
@@ -1,7 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WeddingPlanner.Models
 {
     public class LoginUser
     {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
         
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
     }
 }
